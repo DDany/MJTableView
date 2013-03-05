5ac3093bb67a5c6ac46a7aec5761adb0cb501ebd
Dany <dany1988@163.com>
2013-03-05 11:19:21 +0800

    modify new sample code

---
+++ MJTableViewExample/MJTableViewExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MJTableViewExample
+//
+//  Created by Dany on 13-3-5.
+//  Copyright (c) 2013年 Dany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
